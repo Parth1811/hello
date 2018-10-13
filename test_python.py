6d828b380c8e6304962501ac7f63a6a84399f571
Parth <parthvin@gmail.com>
2018-10-14 01:26:43 +0530

    clearing commit

--- test_python.py
+++ test_python.py
@@ -1,30 +1,74 @@
-import socketio
-import eventlet
-from flask import Flask, render_template
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
-sio = socketio.Server()
-app = Flask(__name__)
+class FileChooserWindow(Gtk.Window):
 
-@app.route('/')
-def index():
-    """Serve the client-side application."""
-    return render_template('index.html')
+    def __init__(self):
+        Gtk.Window.__init__(self, title="FileChooser Example")
 
-@sio.on('connect')
-def connect(sid, environ):
-    print('connect ', sid)
+        box = Gtk.Box(spacing=6)
+        self.add(box)
 
-@sio.on('my message')
-def message(sid, data):
-    print('message ', data)
+        button1 = Gtk.Button("Choose File")
+        button1.connect("clicked", self.on_file_clicked)
+        box.add(button1)
 
-@sio.on('disconnect')
-def disconnect(sid):
-    print('disconnect ', sid)
+        button2 = Gtk.Button("Choose Folder")
+        button2.connect("clicked", self.on_folder_clicked)
+        box.add(button2)
 
-if __name__ == '__main__':
-    # wrap Flask application with socketio's middleware
-    app = socketio.Middleware(sio, app)
+    def on_file_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog("Please choose a file", self,
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
 
-    # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+        self.add_filters(dialog)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def add_filters(self, dialog):
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("Text files")
+        filter_text.add_mime_type("text/plain")
+        dialog.add_filter(filter_text)
+
+        filter_py = Gtk.FileFilter()
+        filter_py.set_name("Python files")
+        filter_py.add_mime_type("text/x-python")
+        dialog.add_filter(filter_py)
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Any files")
+        filter_any.add_pattern("*")
+        dialog.add_filter(filter_any)
+
+    def on_folder_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog("Please choose a folder", self,
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             "Select", Gtk.ResponseType.OK))
+        dialog.set_default_size(800, 400)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Select clicked")
+            print("Folder selected: " + dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+win = FileChooserWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
+
