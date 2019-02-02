686f7b5434cbbbf4c6a2b7a3e213342313ec263c
parth <parthvin@gmail.com>
2019-02-02 16:09:51 +0530

    added closed regresiion function

--- gnr652/assignment1/liner_reg.py
+++ gnr652/assignment1/liner_reg.py
@@ -1,15 +1,26 @@
  #! /usr/bin/python
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 #GLOBAL VARIABLES
-NUM_OF_ITER = 50000
+NUM_OF_ITER = 500000
+LOG_DATA = 1000
 NUM_OF_BATCHES = 10
 ALPHA = 0.005
 LAMBDA = 0.1
 TRAIN_DATA_SIZE = 108
 
-batch_results = []
 data_filepath = 'data/data.csv'
+
+train_loss_history = {
+    'grad_dec' : [],
+    'closed_reg' : []
+}
+test_loss_history = {
+    'grad_dec' : [],
+    'closed_reg' : []
+}
 
 
 def get_data():
@@ -57,41 +68,77 @@ def gradient(X, Y, W):
     grad /= X.shape[0]
     return grad
 
-def train_gradient(x_train, y_train, weights):
+def train_gradient(X, Y, W):
     '''
     This funcntion computes the weights using the gradient decent
     algorithm on the trainning data x_train and y_train
     '''
     for i in range (NUM_OF_ITER):
-        loss = Loss(x_train, y_train, weights)
-        grad = gradient(x_train, y_train, weights)
-        weights -= ALPHA * gradient(x_train, y_train, weights)
+        loss = Loss(X, Y, W)
+        grad = gradient(X, Y, W)
+        W -= ALPHA * grad
 
-        # if(i == 0 or i==NUM_OF_ITER-1):
-        #     test_loss = Loss(x_test, y_test, weights)
-        #     print loss
-        #     print test_loss
-        #     print '--------------'
+        if (i % LOG_DATA == 0):
+            train_loss_history['grad_dec'].append(loss)
 
-    return loss, weights
+    return loss, W
+
+def train_closed_reg(X, Y, tries=10):
+    failed = False
+    try:
+        W = np.dot((np.linalg.inv(np.dot(X.transpose(),X))),np.dot(X.transpose(),Y))
+        loss = Loss(X, Y, W)
+    except:
+        #Handling the singular matrix cases
+        failed = True
+        W, loss = np.random.rand(18), 0
+    # print W
+
+    return loss, W, failed
+
+
+
 
 if __name__ == '__main__':
     data = get_data()
     x_train, y_train, x_test, y_test = partition_test_train_data(data)
 
     #Initializing weights to random values
-    weights = np.random.rand(18)
+    weights_grad = np.random.rand(18)
 
     #iterating for different batches
     for batch_id in range(NUM_OF_BATCHES):
-        loss, weights = train_gradient(x_train, y_train, weights)
-        test_loss = Loss(x_test, y_test, weights)
+        #Finding weights using gradient decent
+        loss_grad, weights_grad = train_gradient(x_train, y_train, weights_grad)
+
+        #Finding wieghts using closed regression
+        closed_failed, counter = True, 0
+        while closed_failed:
+            loss_closed , weights_closed, closed_failed  = train_closed_reg(x_train, y_train)
+            # loss_closed, weights_closed = train_gradient(x_train, y_train, weights_closed)
+            x_train, y_train, x_test, y_test = partition_test_train_data(data)
+            counter += 1
+            if counter > 100:
+                print "Unavle to do Closed Regression"
+                break
+
+        #Finding average loss on test data
+        test_loss_grad = Loss(x_test, y_test, weights_grad)
+        test_loss_closed = Loss(x_test, y_test, weights_closed)
+        test_loss_history['grad_dec'].append(test_loss_grad)
+        test_loss_history['closed_reg'].append(test_loss_closed)
+
         print 'Ongoing batch : %d of %d' % (batch_id + 1, NUM_OF_BATCHES)
-        print 'Train loss : %f Test loss : %f' % (loss, test_loss)
+        print 'Gradient Decent  - train_loss: %.4f test_loss: %.4f' % (loss_grad, test_loss_grad)
+        print 'Closed Regression- train_loss: %.4f test_loss: %.4f tries: %d' % (loss_closed, test_loss_closed, counter)
         print "--------------"
-        batch_results.append(test_loss)
 
         #repartitioning test and train data
         x_train, y_train, x_test, y_test = partition_test_train_data(data)
 
-    print 'Average Loss on Test data : %f ' % (np.array(batch_results).sum()/NUM_OF_BATCHES)
+
+
+    plt.plot(test_loss_history['grad_dec'])
+    plt.plot(test_loss_history['closed_reg'])
+    plt.show()
+    # print 'Average Loss on Test data : %f ' % (np.array(batch_results).sum()/NUM_OF_BATCHES)
