eeb6ca6a76a69e00af04e0537f9c2495b36fe7c3
parth <parthvin@gmail.com>
2020-05-01 15:01:16 +0530

    Added the project scoring files for chiru

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-import logging
-from datetime import datetime
+data_file = open('chiru_data.csv', 'r')
+data = data_file.readlines()
+data_file.close()
 
+data = data[3:]
 
-logging.basicConfig(filename = 'log.txt', level = logging.DEBUG)
-logger = logging.getLogger()
-logger.info("Starting log for " + str(datetime.now()))
+answer_key = [
+    0, 1, 0, 1, 0,
+    1, 0, 1, 0, 1,
+    0, 1, 0, 1, 0,
+    1, 1, 1, 0, 1,
+    1, 1
+]
+
+for i, person_data in enumerate(data):
+    responses = person_data.split(',')[7:29]
+    score = 0
+    for j, response in enumerate(responses):
+        if (answer_key[j] and response == 'Yes') or ((not answer_key[j]) and response == 'No'):
+            score += 1
+    data[i] = person_data[:-2] + ',' + str(score) + '\r\n'
